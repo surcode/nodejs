9aa64b2b41c43e286c0e639d42b044bb2e7880e3
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2021-02-10 20:15:40 +0530

    hit has been added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('working fine');
+    res.send('message has been changed');
 });
 
 const port = process.env.PORT || 3005;
